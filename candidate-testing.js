4f4d0be6231519cfab221587de0ba770f400582f
Joseph Maize <jmaize12@gmail.com>
2021-02-16 08:12:39 +0000

    got rid of .toString() and changed == to !=

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = [ 'Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
+let questions = [ 'Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS ' ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let numberCorrect = 0;
@@ -23,7 +23,7 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (let i = 0; i < questions.length; i++){
-    candidateAnswers.push(input.question(questions[i] + "\nYour Answer: "));
+    candidateAnswers[i] = input.question(questions[i] + "\nYour Answer: ");
     console.log(`Correct Answer: ${correctAnswers[i]}\n`)
 
   }
@@ -33,7 +33,7 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
    for (let i = 0; i < correctAnswers.length; i++){
-    if (correctAnswers[i].toString().toLowerCase() == candidateAnswers[i].toString().toLowerCase()){
+    if (correctAnswers[i].toLowerCase() != candidateAnswers[i].toLowerCase()){
       numberCorrect++
     }
   }
